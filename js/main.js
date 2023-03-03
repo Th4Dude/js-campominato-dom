cd1d4fbaab3fc4e352b776acd2ebe14b2a0c822a
Th4Dude <gallottimaurizio@yahoo.it>
2023-03-03 18:44:03 +0200

    ecco due

--- js/main.js
+++ js/main.js
@@ -5,11 +5,31 @@ const containerCells = document.querySelector('.container-cells')
 /*-------event bottone------- */
 
 let bottone = document.querySelector(".button-01");
-
+let cellNumber ;
+let level ;
 bottone.addEventListener("click", function () {
+    let level = 
     let container = document.querySelector('.container-cells');
     container.classList.toggle("mostra");
     console.log(container)
+    switch (level) {
+        case 2:
+            cellNumber = 81;
+            break;
+
+        case 3:
+            cellNumber = 49;
+            break;
+
+        case 1:
+        default:
+            cellNumber = 100;
+    }
+    for (let i = 1; i <= cellNumber; i++) {
+        const elementoCreato = createCell('div', 'cell', i);
+        containerCellsAppend(containerCells, elementoCreato); 
+        console.log()
+    }
 });
 
 /*-------event bottone------- */
@@ -42,28 +62,11 @@ function containerCellsAppend(container, elemento) {
 
 } */
 
-let cellNumber ;
 
-let level = 2 ;
 
-for (let i = 1; i <= cellNumber; i++) {
-    const elementoCreato = createCell('div', 'cell', i);
-    containerCellsAppend(containerCells, elementoCreato);
-    switch (level) {
-        case 2:
-            cellNumber = 81;
-            break;
 
-        case 3:
-            cellNumber = 49;
-            break;
 
-        case 1:
-        default:
-            cellNumber = 100;
-    }
-    console.log()
-}
+
 
 /* Ciclo for */
 
