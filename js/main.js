caf22c8dc94e1b81a1478c28c8f5bd57389c1ac1
Th4Dude <gallottimaurizio@yahoo.it>
2023-03-04 19:09:54 +0200

    aggiunta bombe e colore

--- js/main.js
+++ js/main.js
@@ -1,84 +1,78 @@
 'Use strict'
 
-
-function creaCella(elemento, classe1, classe2, valore, arrayBomb){
+function creaCella(elemento, classe1, classe2, valore, arrayBomb) {
     const elementoCreato = document.createElement(elemento);
     elementoCreato.classList.add(classe1);
     elementoCreato.classList.add(classe2);
     elementoCreato.innerText = valore;
-
-
-
-    elementoCreato.addEventListener('click', function(){
+    elementoCreato.addEventListener('click', function () {
         elementoCreato.classList.add('blue');
         if (arrayBomb.includes(valore)) {
-
             elementoCreato.classList.add('red');
-            console.log("BOOM! Hai calpestato una bomba.");
-
+            console.log("Hai perso.");
         } else {
             elementoCreato.classList.add('blue');
-            console.log("Non hai calpestato una bomba.");
+            console.log("hai vinto.");
         }
     })
-
+    
     return elementoCreato;
 }
 
-function appendiCella(container, element){
+function appendiCella(container, element) {
     container.append(element);
 }
 
-function randomArray(max) {
+function randomArray(max, bombe) {
     let array = [];
-    while (array.length < 16) {
-      let numeroRandom = Math.floor(Math.random() * max) + 1;
-      if (!array.includes(numeroRandom)) {
-        array.push(numeroRandom);
-      }
+    while (array.length < bombe) {
+        let numeroRandom = Math.floor(Math.random() * max) + 1;
+        if (!array.includes(numeroRandom)) {
+            array.push(numeroRandom);
+        }
     }
     return array;
-  }
-
-
+}
 
 /* choose diff */
 
-function start(){
+function start() {
+
     const difficult = document.querySelector(".difficult").value;
     const scelta = difficult;
     container.innerHTML = '';
 
     let max = 0
+    let bombe = 0
 
-    if(scelta === 'normal'){
-        for(let i = 1; i <= 81; i++){
+    if (scelta === 'normal') {
+        for (let i = 1; i <= 81; i++) {
             max = 81
-            const cella = creaCella('div', 'cella', 'normal', i);
-            appendiCella(container, cella);
+            bombe = 16
         }
-    } else if (scelta === 'hard'){
-        for(let i = 1; i <= 49; i++){
+    } else if (scelta === 'hard') {
+        for (let i = 1; i <= 49; i++) {
             max = 49
-            const cella = creaCella('div', 'cella', 'hard', i);
-            appendiCella(container, cella);
+            bombe = 16
         }
     } else {
-        for(let i = 1; i <= 100; i++){
-
-             max = 100
-            const cella = creaCella('div', 'cella', 'easy', i);
-            appendiCella(container, cella);
+        for (let i = 1; i <= 100; i++) {
+            max = 100
+            bombe = 16
         }
     }
 
+    let arrayBomb = randomArray(max, bombe)
+    console.log(arrayBomb)
+
+    for (let i = 1; i <= max; i++) {
+        const cella = creaCella('div', 'cella', scelta, i, arrayBomb);
+        appendiCella(container, cella);
+    }
 }
 
 const container = document.querySelector(".board");
 const button = document.querySelector(".btn");
-
-  let arrayBomb = randomArray(100)
-  console.log(arrayBomb)
 
 /* btn */
 
